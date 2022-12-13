d977f2c5cb3b7a46ae2243a9f916de49e1dc3da1
pkar <pkar@ieee.org>
2022-12-13 12:15:46 +0100

    feat: get set values

--- Nuget-Configsy/ConfigExtensions.vb
+++ Nuget-Configsy/ConfigExtensions.vb
@@ -1,23 +1,23 @@
-﻿Imports Microsoft.Extensions.Configuration
+﻿Imports MsExtConf = Microsoft.Extensions.Configuration
 
-Partial Module Extensions
+Public Module Extensions
     <Runtime.CompilerServices.Extension()>
     <CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification:="<Pending>")>
-    Public Function AddJsonRwSettings(ByVal configurationBuilder As IConfigurationBuilder, sPathnameLocal As String, sPathnameRoam As String, Optional bReadOnly As Boolean = False) As IConfigurationBuilder
+    Public Function AddJsonRwSettings(ByVal configurationBuilder As MsExtConf.IConfigurationBuilder, sPathnameLocal As String, sPathnameRoam As String, Optional bReadOnly As Boolean = False) As MsExtConf.IConfigurationBuilder
         configurationBuilder.Add(New JsonRwConfigurationSource(sPathnameLocal, sPathnameRoam, bReadOnly))
         Return configurationBuilder
     End Function
 
     <Runtime.CompilerServices.Extension()>
     <CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification:="<Pending>")>
-    Public Function AddIniRelDebugSettings(ByVal configurationBuilder As IConfigurationBuilder, sIniContent As String, bUseDebug As Boolean) As IConfigurationBuilder
+    Public Function AddIniReleaseDebugSettings(ByVal configurationBuilder As MsExtConf.IConfigurationBuilder, sIniContent As String, bUseDebug As Boolean) As MsExtConf.IConfigurationBuilder
         configurationBuilder.Add(New IniDefaultsConfigurationSource(sIniContent, bUseDebug))
         Return configurationBuilder
     End Function
 
     <Runtime.CompilerServices.Extension()>
     <CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification:="<Pending>")>
-    Public Function AddCommandLineRO(ByVal configurationBuilder As IConfigurationBuilder, aArgs As List(Of String)) As IConfigurationBuilder
+    Public Function AddCommandLineRO(ByVal configurationBuilder As MsExtConf.IConfigurationBuilder, aArgs As List(Of String)) As MsExtConf.IConfigurationBuilder
         configurationBuilder.Add(New CommandLineROConfigurationSource(aArgs))
         Return configurationBuilder
     End Function
@@ -25,7 +25,7 @@ Partial Module Extensions
 
     <Runtime.CompilerServices.Extension()>
     <CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification:="<Pending>")>
-    Public Function AddEnvironmentVariablesROConfigurationSource(ByVal configurationBuilder As IConfigurationBuilder, sPrefix As String, oDict As System.Collections.IDictionary) As IConfigurationBuilder
+    Public Function AddEnvironmentVariablesROConfigurationSource(ByVal configurationBuilder As MsExtConf.IConfigurationBuilder, sPrefix As String, oDict As System.Collections.IDictionary) As MsExtConf.IConfigurationBuilder
         configurationBuilder.Add(New EnvironmentVariablesROConfigurationSource(sPrefix, oDict))
         Return configurationBuilder
     End Function
---
+++ Nuget-Configsy/GetSet.vb
@@ -0,0 +1,100 @@
+﻿Imports MsExtConf = Microsoft.Extensions.Configuration
+
+Module GetSet
+
+    Private _settingsGlobal As MsExtConf.IConfigurationRoot
+
+    Public Sub LibInitSettings(settings As MsExtConf.IConfigurationRoot)
+        _settingsGlobal = settings
+    End Sub
+
+    Private Sub SettingsCheckInit()
+        If _settingsGlobal IsNot Nothing Then Return
+
+        Debug.WriteLine("Settings calls without LibInitSettings, using only temporary values")
+        Dim settings As MsExtConf.IConfigurationRoot =
+                (New MsExtConf.ConfigurationBuilder).AddJsonRwSettings(IO.Path.GetTempFileName, Nothing).Build()
+    End Sub
+
+    Public Sub SetSettingsString(sName As String, value As String, Optional bRoam As Boolean = False)
+        SettingsCheckInit()
+
+        If bRoam Then value = "[ROAM]" & value
+
+        _settingsGlobal(sName) = value
+
+    End Sub
+
+    Public Sub SetSettingsInt(sName As String, value As Integer, Optional bRoam As Boolean = False)
+        SetSettingsString(sName, value.ToString(System.Globalization.CultureInfo.InvariantCulture), bRoam)
+    End Sub
+
+    Public Sub SetSettingsBool(sName As String, value As Boolean, Optional bRoam As Boolean = False)
+        SetSettingsString(sName, If(value, "True", "False"), bRoam)
+    End Sub
+
+    Public Sub SetSettingsLong(sName As String, value As Long, Optional bRoam As Boolean = False)
+        SetSettingsString(sName, value.ToString(System.Globalization.CultureInfo.InvariantCulture), bRoam)
+    End Sub
+
+    Public Sub SetSettingsDate(sName As String, value As DateTimeOffset, Optional bRoam As Boolean = False)
+        SetSettingsString(sName, value.ToString("yyyy.MM.dd HH:mm:ss"), bRoam)
+    End Sub
+
+    Public Sub SetSettingsCurrentDate(sName As String, Optional bRoam As Boolean = False)
+        SetSettingsDate(sName, DateTimeOffset.Now, bRoam)
+    End Sub
+
+
+    Private Function GetSettingsNet(sName As String, sDefault As String)
+        SettingsCheckInit()
+        Dim sRetVal As String = _settingsGlobal(sName)
+        If sRetVal IsNot Nothing Then Return sRetVal
+
+        Return sDefault
+        ' https://github.com/dotnet/runtime/blob/main/src/libraries/Microsoft.Extensions.Configuration/src/ConfigurationRoot.cs
+        ' widać że zwraca NULL gdy nie trafi na zmienną nigdzie
+    End Function
+
+    Public Function GetSettingsString(sName As String, Optional sDefault As String = "") As String
+        Return GetSettingsNet(sName, sDefault)
+    End Function
+
+    Public Function GetSettingsInt(sName As String, Optional iDefault As Integer = 0) As Integer
+        Dim sRetVal As String = GetSettingsNet(sName, iDefault.ToString(System.Globalization.CultureInfo.InvariantCulture))
+        Dim iRetVal As Integer = 0
+        If Integer.TryParse(sRetVal, Globalization.NumberStyles.Integer, Globalization.CultureInfo.InvariantCulture, iRetVal) Then
+            Return iRetVal
+        End If
+        Return iDefault
+    End Function
+
+    Public Function GetSettingsBool(sName As String, Optional bDefault As Boolean = False) As Boolean
+        Dim sRetVal As String = GetSettingsNet(sName, If(bDefault, "True", "False"))
+        If sRetVal.ToLower = "true" Then Return True
+        Return False
+    End Function
+
+    Public Function GetSettingsLong(sName As String, Optional iDefault As Long = 0) As Long
+        Dim sRetVal As String = GetSettingsNet(sName, iDefault.ToString(System.Globalization.CultureInfo.InvariantCulture))
+        Dim iRetVal As Long = 0
+        If Long.TryParse(sRetVal, Globalization.NumberStyles.Integer, Globalization.CultureInfo.InvariantCulture, iRetVal) Then
+            Return iRetVal
+        End If
+        Return iDefault
+    End Function
+
+    Public Function GetSettingsDate(sName As String, Optional sDefault As String = "") As DateTimeOffset
+        If sDefault = "" Then sDefault = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss")
+        Dim sRetVal As String = GetSettingsNet(sName, sDefault)
+        Dim dRetVal As DateTimeOffset
+        If DateTimeOffset.TryParseExact(sRetVal, {"yyyy.MM.dd HH:mm:ss"},
+                             Globalization.CultureInfo.InvariantCulture.DateTimeFormat,
+                             Globalization.DateTimeStyles.AllowWhiteSpaces, dRetVal) Then
+            Return dRetVal
+        End If
+
+        Return DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss")
+    End Function
+
+End Module
