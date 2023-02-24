1e20f40f12423244651532a4ff165bf83bf6c208
pkar <pkar@ieee.org>
2023-02-24 17:43:07 +0100

    feat: Nuget BT debugs

---
+++ Nuget-Uwp-BTdump/BluetoothExtensions.vb
@@ -0,0 +1,260 @@
+﻿
+Imports pkar.DotNetExtensions
+
+Partial Public Module BTextensions
+
+    ''' <summary>
+    ''' Create string with dumped IBuffer bytes as hex
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    Public Function ToDebugString(ByVal oBuf As Windows.Storage.Streams.IBuffer, iMaxLen As Integer) As String
+        Dim sRet As String = oBuf.Length & ": "
+        Dim oArr As Byte() = oBuf.ToArray
+
+        For i As Integer = 0 To Math.Min(oBuf.Length - 1, iMaxLen)
+            sRet = sRet & oArr.ElementAt(i).ToString("X2") & " "
+        Next
+
+        Return sRet & vbCrLf
+    End Function
+
+    ''' <summary>
+    ''' Create string with dumped IBuffer bytes as hex, maximum 16 bytes
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    <Windows.Foundation.Metadata.DefaultOverload>
+    Public Function ToDebugString(ByVal oBuf As Windows.Storage.Streams.IBuffer) As String
+        Return oBuf.ToDebugString(16)
+    End Function
+
+    ''' <summary>
+    ''' Create string with dumped BluetoothLEAdvertisement data
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    Public Function ToDebugString(ByVal oAdv As Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisement) As String
+
+        If oAdv Is Nothing Then
+            Return "ERROR: Advertisement is Nothing, unmoglich!"
+        End If
+
+        Dim sRet As String = ""
+
+        If oAdv.DataSections IsNot Nothing Then
+            sRet = sRet & "Adverisement, number of data sections: " & oAdv.DataSections.Count & vbCrLf
+            For Each oItem As Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementDataSection In oAdv.DataSections
+                sRet = sRet & " DataSection: " & oItem.Data.ToDebugString(32)
+            Next
+        End If
+
+        If oAdv.Flags IsNot Nothing Then sRet = sRet & "Adv.Flags: " & CInt(oAdv.Flags) & vbCrLf
+
+        sRet = sRet & "Adv local name: " & oAdv.LocalName & vbCrLf
+
+        If oAdv.ManufacturerData IsNot Nothing Then
+            For Each oItem As Windows.Devices.Bluetooth.Advertisement.BluetoothLEManufacturerData In oAdv.ManufacturerData
+                sRet = sRet & " ManufacturerData.Company: " & oItem.CompanyId & vbCrLf
+                sRet = sRet & " ManufacturerData.Data: " & oItem.Data.ToDebugString(32) & vbCrLf
+            Next
+        End If
+
+        If oAdv.ServiceUuids IsNot Nothing Then
+            For Each oItem As Guid In oAdv.ServiceUuids
+                sRet = sRet & " service " & oItem.ToString & vbCrLf
+            Next
+        End If
+
+        Return sRet
+    End Function
+
+
+    ''' <summary>
+    ''' Create string with dumped GattDescriptor data
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    Public Function ToDebugStringAsync(ByVal oDescriptor As Windows.Devices.Bluetooth.GenericAttributeProfile.GattDescriptor) As IAsyncOperation(Of String)
+        Return oDescriptor.ToDebugStringAsyncTask.AsAsyncOperation
+    End Function
+
+    <Runtime.CompilerServices.Extension()>
+    Private Async Function ToDebugStringAsyncTask(ByVal oDescriptor As Windows.Devices.Bluetooth.GenericAttributeProfile.GattDescriptor) As Task(Of String)
+        Dim sRet As String
+
+        sRet = "      descriptor: " & oDescriptor.Uuid.ToString & vbTab & oDescriptor.Uuid.AsGattReservedDescriptorName & vbCrLf
+        Dim oRdVal = Await oDescriptor.ReadValueAsync
+        If oRdVal.Status = Windows.Devices.Bluetooth.GenericAttributeProfile.GattCommunicationStatus.Success Then
+            Dim oVal = oRdVal.Value
+            sRet = sRet & oVal.ToArray.ToDebugString(8) & vbCrLf
+        Else
+            sRet = sRet & "      ReadValueAsync status = " & oRdVal.Status.ToString & vbCrLf
+        End If
+        Return sRet
+    End Function
+
+
+    ''' <summary>
+    ''' Create string with dumped GattCharacteristicProperties flags
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    Public Function ToDebugString(ByVal oProp As Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties) As String
+
+        Dim sRet As String = "      CharacteristicProperties: "
+
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.Read) Then
+            sRet &= "[read] "
+        End If
+
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.AuthenticatedSignedWrites) Then
+            sRet &= "[AuthenticatedSignedWrites] "
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.Broadcast) Then
+            sRet &= "[broadcast] "
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.Indicate) Then
+            sRet &= "[indicate] "
+            ' bCanRead = False
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.None) Then
+            sRet &= "[NONE] "
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.Notify) Then
+            sRet &= "[notify] "
+            ' bCanRead = False
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.ReliableWrites) Then
+            sRet &= "[reliableWrite] "
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.Write) Then
+            sRet &= "[write] "
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.WritableAuxiliaries) Then
+            sRet &= "[WritableAuxiliaries] "
+        End If
+        If oProp.HasFlag(Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristicProperties.WriteWithoutResponse) Then
+            sRet &= "[writeNoResponse] "
+        End If
+
+        Return sRet
+    End Function
+
+    ''' <summary>
+    ''' Create string with dumped GattCharacteristic data
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    Public Function ToDebugStringAsync(ByVal oChar As Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic) As IAsyncOperation(Of String)
+        Return oChar.ToDebugStringAsyncTask.AsAsyncOperation
+    End Function
+    <Runtime.CompilerServices.Extension()>
+    Private Async Function ToDebugStringAsyncTask(ByVal oChar As Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic) As Task(Of String)
+
+
+        Dim sRet As String = "      CharacteristicProperties: " & oChar.CharacteristicProperties.ToDebugString & vbCrLf
+        Dim bCanRead As Boolean = False
+        If sRet.Contains("[read]") Then bCanRead = True
+        ' ewentualnie wygaszenie gdy:
+        'sProp &= "[indicate] "
+        ' bCanRead = False
+        '   sProp &= "[notify] "
+        ' bCanRead = False
+
+
+        Dim oDescriptors = Await oChar.GetDescriptorsAsync
+        If oDescriptors Is Nothing Then Return sRet
+
+        If oDescriptors.Status <> Windows.Devices.Bluetooth.GenericAttributeProfile.GattCommunicationStatus.Success Then
+            sRet = sRet & "      GetDescriptorsAsync.Status = " & oDescriptors.Status.ToString & vbCrLf
+            Return sRet
+        End If
+
+
+        For Each oDescr In oDescriptors.Descriptors
+            sRet = sRet & Await oDescr.ToDebugStringAsync & vbCrLf
+        Next
+
+        If bCanRead Then
+            Dim oRd = Await oChar.ReadValueAsync()
+            If oRd.Status <> Windows.Devices.Bluetooth.GenericAttributeProfile.GattCommunicationStatus.Success Then
+                sRet = sRet & "ReadValueAsync.Status=" & oRd.Status & vbCrLf
+            Else
+                sRet = sRet & "      characteristic data (read):" & vbCrLf
+                sRet = sRet & oRd.Value.ToArray.ToDebugString(8) & vbCrLf
+            End If
+
+        End If
+
+        Return sRet
+    End Function
+
+    ''' <summary>
+    ''' Create string with dumped GattDeviceService data
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    Public Function ToDebusGtringAsync(ByVal oServ As Windows.Devices.Bluetooth.GenericAttributeProfile.GattDeviceService) As IAsyncOperation(Of String)
+        Return oServ.ToDebusGtringAsyncTask.AsAsyncOperation
+    End Function
+    <Runtime.CompilerServices.Extension()>
+    Private Async Function ToDebusGtringAsyncTask(ByVal oServ As Windows.Devices.Bluetooth.GenericAttributeProfile.GattDeviceService) As Task(Of String)
+
+
+        If oServ Is Nothing Then Return ""
+
+        Dim oChars = Await oServ.GetCharacteristicsAsync
+        If oChars Is Nothing Then Return ""
+        If oChars.Status <> Windows.Devices.Bluetooth.GenericAttributeProfile.GattCommunicationStatus.Success Then
+            Return "    GetCharacteristicsAsync.Status = " & oChars.Status.ToString
+        End If
+
+        Dim sRet As String = ""
+        For Each oChr In oChars.Characteristics
+            sRet = sRet & vbCrLf & "    characteristic: " & oChr.Uuid.ToString & oChr.Uuid.AsGattReservedCharacteristicName & vbCrLf
+            sRet = sRet & Await oChr.ToDebugStringAsync & vbCrLf
+        Next
+
+        Return sRet
+    End Function
+
+    ''' <summary>
+    ''' Create string with dumped BluetoothLEDevice data
+    ''' </summary>
+    <Runtime.CompilerServices.Extension()>
+    <Windows.Foundation.Metadata.DefaultOverload>
+    Public Function ToDebugStringAsync(ByVal oDevice As Windows.Devices.Bluetooth.BluetoothLEDevice) As IAsyncOperation(Of String)
+        Return oDevice.ToDebugStringAsyncTask.AsAsyncOperation
+    End Function
+    <Runtime.CompilerServices.Extension()>
+    Private Async Function ToDebugStringAsyncTask(ByVal oDevice As Windows.Devices.Bluetooth.BluetoothLEDevice) As Task(Of String)
+
+
+        Dim sRet As String = ""
+
+        sRet = sRet & "DebugBTdevice, data dump:" & vbCrLf
+        sRet = sRet & "Device name: " & oDevice.Name & vbCrLf
+        sRet = sRet & "MAC address: " & oDevice.BluetoothAddress.ToHexBytesString & vbCrLf
+        sRet = sRet & "Connection status: " & oDevice.ConnectionStatus.ToString & vbCrLf
+
+        Dim oDAI = oDevice.DeviceAccessInformation
+        sRet = sRet & vbCrLf & "DeviceAccessInformation:" & vbCrLf
+        sRet = sRet & "  CurrentStatus: " & oDAI.CurrentStatus.ToString & vbCrLf
+
+        Dim oDApperr = oDevice.Appearance
+        sRet = sRet & vbCrLf & "Appearance:" & vbCrLf
+        sRet = sRet & "  Category: " & oDApperr.Category & vbCrLf
+        sRet = sRet & "  Subcategory: " & oDApperr.SubCategory & vbCrLf
+
+        sRet = sRet & "Services: " & oDApperr.SubCategory & vbCrLf
+
+        Dim oSrv = Await oDevice.GetGattServicesAsync
+        If oSrv.Status <> Windows.Devices.Bluetooth.GenericAttributeProfile.GattCommunicationStatus.Success Then
+            sRet = sRet & "  GetGattServicesAsync.Status = " & oSrv.Status.ToString & vbCrLf
+            Return sRet
+        End If
+
+        For Each oSv In oSrv.Services
+            sRet = sRet & vbCrLf & "  service: " & oSv.Uuid.ToString & vbTab & vbTab & oSv.Uuid.AsGattReservedServiceName & vbCrLf
+            sRet = sRet & Await oSv.ToDebusGtringAsync & vbCrLf
+        Next
+
+        Return sRet
+
+    End Function
+
+End Module
---
+++ Nuget-Uwp-BTdump/BluetoothHelpers.vb
@@ -0,0 +1,261 @@
+﻿
+Imports pkar.DotNetExtensions
+Imports Winradio = Windows.Devices.Radios
+Imports WinBT = Windows.Devices.Bluetooth
+Imports System.Reflection.PortableExecutable
+
+Public NotInheritable Class BThelpers
+
+#Region "BT radio"
+
+    ''' <summary>
+    ''' Checks if Bluetooth is available
+    ''' </summary>
+    ''' <returns>-1 means no radio, 0 means radio is present, but off, and 1 means radio is present and on</returns>
+    Public Shared Function IsBTavailableAsync() As IAsyncOperation(Of Integer)
+        Return IsBTavailableAsyncTask.AsAsyncOperation
+    End Function
+
+    Private Shared Async Function IsBTavailableAsyncTask() As Task(Of Integer)
+
+        Dim oRadios As IReadOnlyList(Of Winradio.Radio) = Await Winradio.Radio.GetRadiosAsync()
+
+        Dim bHasBT As Boolean = False
+
+        For Each oRadio As Winradio.Radio In oRadios
+            If oRadio.Kind = Winradio.RadioKind.Bluetooth Then
+                If oRadio.State = Winradio.RadioState.On Then Return 1
+                bHasBT = True
+            End If
+        Next
+
+        If bHasBT Then Return 0
+        Return -1
+    End Function
+
+    ''' <summary>
+    ''' Tries to set Bluetooth state to bOn (True: on, False: off). App should have devCapabilities "Radios" set in manifest.
+    ''' This should be called from UI thread, as 
+    ''' </summary>
+    ''' <param name="bOn">True if we want to switch Bluetooth on, False if switching off</param>
+    ''' <returns>True if switching is successful (or not needed)</returns>
+    Public Shared Function TrySwitchBTAsync(bOn As Boolean) As IAsyncOperation(Of Boolean)
+        Return TrySwitchBTAsyncTask(bOn).AsAsyncOperation
+    End Function
+    Private Shared Async Function TrySwitchBTAsyncTask(bOn As Boolean) As Task(Of Boolean)
+        Dim iCurrState As Integer = Await IsBTavailableAsync()
+        If iCurrState = -1 Then Return False
+
+        ' jeśli nie trzeba przełączać... 
+        If bOn AndAlso iCurrState = 1 Then Return True
+        If Not bOn AndAlso iCurrState = 0 Then Return True
+
+        Try
+            ' czy mamy prawo przełączyć? (devCap=radios)
+            Dim result222 As Winradio.RadioAccessStatus = Await Winradio.Radio.RequestAccessAsync()
+            If result222 <> Winradio.RadioAccessStatus.Allowed Then Return False
+        Catch ex As Exception
+            ' no permission
+            Return False
+        End Try
+
+
+        Dim radios As IReadOnlyList(Of Winradio.Radio) = Await Winradio.Radio.GetRadiosAsync()
+
+        For Each oRadio As Winradio.Radio In radios
+            If oRadio.Kind = Winradio.RadioKind.Bluetooth Then
+                Dim oStat As Winradio.RadioAccessStatus
+                If bOn Then
+                    oStat = Await oRadio.SetStateAsync(Winradio.RadioState.On)
+                Else
+                    oStat = Await oRadio.SetStateAsync(Winradio.RadioState.Off)
+                End If
+                If oStat <> Winradio.RadioAccessStatus.Allowed Then Return False
+            End If
+        Next
+
+        Return True
+    End Function
+
+#End Region
+
+
+#Region "Bluetooth debugs"
+
+    ''' <summary>
+    ''' This is "last error message" from GetDeviceServiceCharacteristic
+    ''' </summary>
+    Public Shared Property errorMessage As String = ""
+
+#Region "Device"
+
+    ''' <summary>
+    ''' Get Device with MAC address, or set errorMessage
+    ''' </summary>
+    ''' <param name="deviceMAC"></param>
+    ''' <returns>Device, or NULL in case of error. See errorMessage for error message.</returns>
+    Public Shared Function GetDeviceAsync(deviceMAC As ULong) As IAsyncOperation(Of WinBT.BluetoothLEDevice)
+        Return GetDeviceAsyncTask(deviceMAC).AsAsyncOperation
+    End Function
+    Private Shared Async Function GetDeviceAsyncTask(deviceMAC As ULong) As Task(Of WinBT.BluetoothLEDevice)
+
+        errorMessage = ""
+
+        Dim oDev As WinBT.BluetoothLEDevice
+        oDev = Await WinBT.BluetoothLEDevice.FromBluetoothAddressAsync(deviceMAC)
+        If oDev Is Nothing Then
+            errorMessage = "GetDeviceServiceCharacteristic called, cannot get device for deviceMAC = " & deviceMAC.ToHexBytesString
+            Return Nothing
+        End If
+
+        Return oDev
+    End Function
+
+#End Region
+
+#Region "Service"
+    ''' <summary>
+    ''' Get GattDeviceService, or set errorMessage. Similar to obsoleted device.GetGattService 
+    ''' </summary>
+    ''' <returns>Service, or NULL in case of error. See errorMessage for error message.</returns>
+    Public Shared Function GetServiceAsync(oDevice As WinBT.BluetoothLEDevice,
+                                      serviceGuid As String) As IAsyncOperation(Of WinBT.GenericAttributeProfile.GattDeviceService)
+        Return GetServiceAsyncTask(oDevice, serviceGuid).AsAsyncOperation
+    End Function
+
+    Private Shared Async Function GetServiceAsyncTask(oDevice As WinBT.BluetoothLEDevice,
+                                      serviceGuid As String) As Task(Of IAsyncOperation(Of WinBT.GenericAttributeProfile.GattDeviceService))
+
+        errorMessage = ""
+
+        If oDevice Is Nothing Then
+            errorMessage = "GetDeviceServiceCharacteristic called with oDevice = null"
+            Return Nothing
+        End If
+
+        Dim oSrv = Await oDevice.GetGattServicesAsync
+        If oSrv.Status <> WinBT.GenericAttributeProfile.GattCommunicationStatus.Success Then
+            errorMessage = "GetDeviceServiceCharacteristic:GetGattServicesAsync.Status = " & oSrv.Status.ToString
+            Return Nothing
+        End If
+
+        Dim oSvc As WinBT.GenericAttributeProfile.GattDeviceService = Nothing
+        For Each oSv In oSrv.Services
+            If oSv.Uuid.ToString = serviceGuid.ToLower Then
+                oSvc = oSv
+            End If
+        Next
+        If oSvc Is Nothing Then
+            errorMessage = "GetDeviceServiceCharacteristic: cannot find service " & serviceGuid
+            Return Nothing
+        End If
+
+        Return oSvc
+    End Function
+
+
+    ''' <summary>
+    ''' Get GattDeviceService, or set errorMessage. Similar to obsoleted device.GetGattService 
+    ''' </summary>
+    ''' <returns>Service, or NULL in case of error. See errorMessage for error message.</returns>
+    Public Shared Function GetDeviceServiceAsync(deviceMAC As ULong,
+                                      serviceGuid As String) As IAsyncOperation(Of WinBT.GenericAttributeProfile.GattDeviceService)
+        Return GetDeviceServiceAsyncTask(deviceMAC, serviceGuid).AsAsyncOperation
+    End Function
+
+    Private Shared Async Function GetDeviceServiceAsyncTask(deviceMAC As ULong,
+                                      serviceGuid As String) As Task(Of WinBT.GenericAttributeProfile.GattDeviceService)
+
+        errorMessage = ""
+
+        Dim oDev As WinBT.BluetoothLEDevice = Await GetDeviceAsync(deviceMAC)
+        If oDev Is Nothing Then Return Nothing
+
+        Return Await GetServiceAsyncTask(oDev, serviceGuid)
+
+    End Function
+
+#End Region
+
+#Region "Characteristic"
+
+    ''' <summary>
+    ''' Get Characteristic from Service from device with MAC address
+    ''' </summary>
+    ''' <returns>Characteristic, or NULL in case of error. See errorMessage for error message.</returns>
+    Public Shared Function GetCharacteristicAsync(service As WinBT.GenericAttributeProfile.GattDeviceService,
+                                      charGuid As String) As IAsyncOperation(Of WinBT.GenericAttributeProfile.GattCharacteristic)
+        Return GetCharacteristicAsyncTask(service, charGuid).AsAsyncOperation
+    End Function
+
+    Private Shared Async Function GetCharacteristicAsyncTask(service As WinBT.GenericAttributeProfile.GattDeviceService,
+                                      charGuid As String) As Task(Of WinBT.GenericAttributeProfile.GattCharacteristic)
+
+        errorMessage = ""
+
+        Dim oChars = Await service.GetCharacteristicsAsync
+
+        If oChars Is Nothing Then
+            errorMessage = "GetDeviceServiceCharacteristic:GetCharacteristicsAsync = null"
+            Return Nothing
+        End If
+
+        If oChars.Status <> WinBT.GenericAttributeProfile.GattCommunicationStatus.Success Then
+            errorMessage = "GetDeviceServiceCharacteristic:GetCharacteristicsAsync.Status = " & oChars.Status.ToString
+            Return Nothing
+        End If
+
+        For Each oChr In oChars.Characteristics
+            If oChr.Uuid.ToString = charGuid.ToLower Then Return oChr
+        Next
+
+        Return Nothing
+    End Function
+
+    ''' <summary>
+    ''' Get Characteristic from ServiceGUID from device
+    ''' </summary>
+    ''' <returns>Characteristic, or NULL in case of error. See errorMessage for error message.</returns>
+    Public Shared Function GetServiceCharacteristicAsync(oDevice As WinBT.BluetoothLEDevice,
+                                      serviceGuid As String, charGuid As String) As IAsyncOperation(Of WinBT.GenericAttributeProfile.GattCharacteristic)
+        Return GetServiceCharacteristicAsyncTask(oDevice, serviceGuid, charGuid).AsAsyncOperation
+    End Function
+
+    Private Shared Async Function GetServiceCharacteristicAsyncTask(oDevice As WinBT.BluetoothLEDevice,
+                                      serviceGuid As String, charGuid As String) As Task(Of WinBT.GenericAttributeProfile.GattCharacteristic)
+
+        errorMessage = ""
+
+        Dim service As WinBT.GenericAttributeProfile.GattDeviceService = Await GetServiceAsync(oDevice, serviceGuid)
+        If service Is Nothing Then Return Nothing
+
+        Return Await GetCharacteristicAsync(service, charGuid)
+
+    End Function
+
+
+    ''' <summary>
+    ''' Get Characteristic from ServiceGUID from deviceMAC
+    ''' </summary>
+    ''' <returns>Characteristic, or NULL in case of error. See errorMessage for error message.</returns>
+    Public Shared Function GetDeviceServiceCharacteristicAsync(deviceMAC As ULong,
+                                      serviceGuid As String, charGuid As String) As IAsyncOperation(Of WinBT.GenericAttributeProfile.GattCharacteristic)
+        Return GetDeviceServiceCharacteristicAsyncTask(deviceMAC, serviceGuid, charGuid).AsAsyncOperation
+    End Function
+
+    Private Shared Async Function GetDeviceServiceCharacteristicAsyncTask(deviceMAC As ULong,
+                                      serviceGuid As String, charGuid As String) As Task(Of WinBT.GenericAttributeProfile.GattCharacteristic)
+
+        errorMessage = ""
+
+        Dim service As WinBT.GenericAttributeProfile.GattDeviceService = Await GetDeviceServiceAsyncTask(deviceMAC, serviceGuid)
+        If service Is Nothing Then Return Nothing
+
+        Return Await GetCharacteristicAsync(service, charGuid)
+
+    End Function
+
+#End Region
+#End Region
+
+End Class
