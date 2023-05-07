ad20d641650f264ce9821f27e42b0c8cbb5f0bef
pkar <pkar@ieee.org>
2023-05-07 14:11:24 +0200

    feat: Nuget wpf.config v1.0.0

---
+++ pkar.WPF.Configs/ConfigsWPF.vb
@@ -0,0 +1,18 @@
+﻿
+
+Public Module WpfConfig
+
+    ''' <summary>
+    ''' initialization of library - as a wrapper to pkar.NetConfigs. Used sources: (1) INI source; (2) Environment variables, prefixed with appname; (3) UWP ApplicationData.Current roaming and local; (4) JSON source, also roaming and local; (5) command line arguments
+    ''' </summary>
+    ''' <param name="sINIcontent">INI source; Content of INI file, NULL or "" if you don't want to use it</param>
+    ''' <param name="bIniUseDebug">True if [debug] section is to be used ([main] is used always)</param>
+    Public Sub InitSettings(sIniContent As String, bIniUseDebug As Boolean)
+
+        ' use cmdline, envVars, JSON local+roam (no temp)
+        pkar.NetConfigs.InitSettings(sIniContent, bIniUseDebug, True, Nothing, False, True, True)
+
+    End Sub
+
+End Module
+
